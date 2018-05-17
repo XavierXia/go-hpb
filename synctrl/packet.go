78fed761b857041e82c954d7c76b0f6882423833
xujl <839840824@qq.com>
2018-05-17 16:17:32 +0800

    packet factory

--- synctrl/packet.go
+++ synctrl/packet.go
@@ -13,28 +13,81 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-hpb. If not, see <http://www.gnu.org/licenses/>.
+
 package synctrl
 
 import (
-//"crypto/rand"
-//"errors"
-//"fmt"
-//"math"
-//"math/big"
-//"sync"
-//"sync/atomic"
-//"time"
-//
-//hpbinter "github.com/hpb-project/go-hpb/interface"
-//"github.com/hpb-project/go-hpb/common"
-//"github.com/hpb-project/go-hpb/data/types"
-//"github.com/hpb-project/go-hpb/data/storage"
-//"github.com/hpb-project/go-hpb/event"
-//"github.com/hpb-project/go-hpb/common/log"
-//"github.com/hpb-project/go-hpb/common/constant"
-//"github.com/rcrowley/go-metrics"
+	"github.com/hpb-project/go-hpb/data/types"
 )
 
-type packet struct {
+const (
+	Pk_Header          = 0x00
+	Pk_Body            = 0x01
+	Pk_Receipt         = 0x02
+	Pk_State           = 0x03
+)
 
+type pkFactory struct {
 }
+
+func (this pkFactory) Create(tp int) packet {
+	switch tp {
+	case Pk_Header:
+		return new(pk_header)
+	case Pk_Body:
+		return new(pk_body)
+	case Pk_Receipt:
+		return new(pk_receipt)
+	case Pk_State:
+		return new(pk_state)
+	default:
+		return nil
+	}
+}
+
+type packet interface {
+	PeerId() string
+	Items() int
+}
+
+// headerPack is a batch of block headers returned by a peer.
+type pk_header struct {
+	peerId  string
+	headers []*types.Header
+}
+
+func (p *pk_header) PeerId() string { return p.peerId }
+func (p *pk_header) Items() int     { return len(p.headers) }
+
+// bodyPack is a batch of block bodies returned by a peer.
+type pk_body struct {
+	peerId       string
+	transactions [][]*types.Transaction
+	uncles       [][]*types.Header
+}
+
+func (p *pk_body) PeerId() string { return p.peerId }
+func (p *pk_body) Items() int {
+	if len(p.transactions) <= len(p.uncles) {
+		return len(p.transactions)
+	}
+	return len(p.uncles)
+}
+
+// receiptPack is a batch of receipts returned by a peer.
+type pk_receipt struct {
+	peerId   string
+	receipts [][]*types.Receipt
+}
+
+func (p *pk_receipt) PeerId() string { return p.peerId }
+func (p *pk_receipt) Items() int     { return len(p.receipts) }
+
+// statePack is a batch of states returned by a peer.
+type pk_state struct {
+	peerId string
+	states [][]byte
+}
+
+func (p *pk_state) PeerId() string { return p.peerId }
+func (p *pk_state) Items() int     { return len(p.states) }
